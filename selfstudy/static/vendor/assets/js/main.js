01249135df5fbe300e910f34366f10333478bf0c
Justin859 <justinhammond859@yahoo.com>
2017-12-09 17:43:54 +0200

    Changed lyout for landing page. Added plugin for simple side nav for mobile and desktop.

--- selfstudy/static/vendor/assets/js/main.js
+++ selfstudy/static/vendor/assets/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-    $("#floatMenu").sticky({topSpacing:0});
-    $("#floatTab").sticky({topSpacing:0});
+
+$('#cd-menu-trigger').click(function() {
+    if ($('#cd-menu-trigger').hasClass('is-clicked')) {
+        $('#cd-menu-trigger').css('background-color', 'rgb(235,148,79)');
+    } else {
+        $('#cd-menu-trigger').css('background-color', 'rgb(28,78,200)');
+    }
+})
+
 });
